202a2b975a6ff44cdbe0ffc01a00bc00b3bc981d
DemyanGvr <xd-1997_2010@mail.ru>
2025-03-07 18:39:05 +0500

    Add files via upload

---
+++ bot/bot.py
@@ -0,0 +1,29 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+TOKEN = "8107472199:AAFMbroM47_8tzWpXVmiXKU27ssd8tzKyJk"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+# Создаём клавиатуру с 2 кнопками
+keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="🔗 Регистрация со спецпредложением")],
+        [KeyboardButton(text="💬 Приватный чат с инсайдерской схемой")]
+    ],
+    resize_keyboard=True
+)
+
+# Обрабатываем команду /start
+@dp.message()
+async def send_welcome(message: types.Message):
+    if message.text == "/start":
+        await message.answer("👋 Добро пожаловать! Выберите один из вариантов:", reply_markup=keyboard)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
